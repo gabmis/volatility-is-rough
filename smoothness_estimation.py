96161f27a50bb16af04b44f08cd76bc7c36d9e9b
gabmis <gabriel.misrachi@gmail.com>
2017-12-11 14:32:58 +0100

    "update smoothness"

--- smoothness_estimation.py
+++ smoothness_estimation.py
@@ -4,22 +4,16 @@ import matplotlib.pyplot as plt
 
 # we compute the empirical q-variation of the log volatility (proxied with realized var)
 def m(motion, t, q, delta):
+    if delta == 1:
+        return np.mean(np.power(np.abs(np.log(motion[1:]) - np.log(motion[:-1])), q))
     n = int(np.floor(t / delta))
-    nb_starting_points = int(t - n * delta) + 1
-    index = np.arange(n) * int(delta)
-    # if one starting possible starting point
-    if nb_starting_points == 1:
-        log_fbm = np.log(motion[index])
-        # directly compute value
-        return np.mean(np.power(np.abs(log_fbm[1:] - log_fbm[:-1]), q))
-    # if multiple possible starting points
-    else:
-        # compute for all starting points
-        starts = np.arange(nb_starting_points)
-        copies_motion = np.stack([motion[start:][index] for start in starts])
-        log_fbm = np.log(copies_motion)
-        # take the mean
-        return np.mean(np.power(np.abs(log_fbm[:, 1:] - log_fbm[:, :-1]), q))
+    index = np.arange(n - 1) * int(delta)
+    starts = np.arange(delta - 1)
+    # compute for all starting points
+    copies_motion = np.stack([motion[start:][index] for start in starts])
+    log_fbm = np.log(copies_motion)
+    # take the mean
+    return np.mean(np.power(np.abs(log_fbm[:, 1:] - log_fbm[:, :-1]), q))
 
 
 # we plot the the values of log(m(q, delta)) against log(delta)
@@ -37,7 +31,7 @@ def plotVar(motion, t):
             logM = np.log(m(motion, t, arr_q[i], arr_delta[j]))
             arr_res.append(logM)
         # plot the results
-        plt.plot(logDelta, arr_res,color=colors[i], label=" q = {} ".format(arr_q[i]))
+        plt.scatter(logDelta, arr_res, s=0.5, color=colors[i], label=" q = {} ".format(arr_q[i]))
     plt.legend(loc="best")
     plt.show()
 
